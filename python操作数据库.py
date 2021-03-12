872f071e7b351dbe492d9349c99fe4f97ad5c103
涛旭 邹 <1595206644@qq.com>
2021-03-12 17:23:27 +0800

    pass

--- python操作数据库.py
+++ python操作数据库.py
@@ -1,16 +1,6 @@
-import random
-
 import pymysql
 
-file_sead = open('name.txt', 'r', encoding='utf8')
-school_major_all = ['信息工程系', '海洋工程系', '机电工程系', '经济管理系', '艺术与设计系', '外语外贸系', '人文社科系']
-name_all = file_sead.read().split(',')
-age_all = [18, 19, 20, 21]
-file_sead.close()
-
-# 连接mysql；失败返回连接数据库失败，成功返回输出连接数据库成功。
-
-# 连接mysql的方法 connect('ip','user','password','dbname')
+# 连接数据库
 connection = pymysql.connect(
     host='localhost',  # IP，MySQL数据库服务器IP地址
     port=3306,  # 端口，默认3306，可以不输入
@@ -18,26 +8,36 @@ connection = pymysql.connect(
     password='root',  # 数据库登录密码
     database='taoxu',  # 要连接的数据库
     charset='utf8'  # 字符集，注意不是utf-8
-
 )
+
 # 使用cursor()方法创建一个游标对象cursor
 cursor = connection.cursor()
 
-for i in range(1, 3000):
-    student_id = 2018328100 + i
-    name = random.choice(name_all)
-    age = random.choice(age_all)
-    in_date = '2018-9-1'
-    out_date = '2021-6-30'
-    major = random.choice(school_major_all)  # 随机系别
-    insert_sql = 'insert into student_text(id,student_id,name,age,in_date,out_date,major) values ({},{},\'{}\',{},\'{}\',\'{}\',\'{}\');'.format(
-        i, student_id, name, age, in_date, out_date, major)
-    cursor.execute(insert_sql)
-    cursor.connection.commit()
+# 创建数据库
+# cursor.execute('create database mydatabese')
 
-select_sql = 'select * from student_text'
-cursor.execute(select_sql)
-result_one = cursor.fetchone()
-result_two = cursor.fetchone()
-print(result_one)
-print(result_two)
+# 创建表
+# cursor.execute("CREATE TABLE customers (name varchar(255),address varchar(255))")
+
+# 修改表操作
+# cursor.execute('alter table customers add id int primary key auto_increment')
+# 修改costomers表 添加id字段 类型为int 主键 自增
+
+# 查询并打印数据库中的所有表
+# cursor.execute('show tables')
+# for table in cursor:
+#     print(table)
+
+# 插入数据
+sql = 'insert into student_text(id,student_id,name,age,in_date,out_date,major) values (%(id)s,%(student_id)s,%(name)s,%(age)s,%(in_date)s,%(out_date)s,%(major)s)'
+sql_values = {
+    'id': 3001,
+    'student_id': 2018331101,
+    'name': '憨憨梨',
+    'age': 18,
+    'in_date': '2018-9-1',
+    'out_date': '2021-6-30',
+    'major': '信息工程系'
+}  # 以字典的形式填入数据
+cursor.execute(sql, sql_values)
+cursor.connection.commit()  # 请记得添加这句提交代码
